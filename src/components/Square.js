46e7ed28dbb184c788a8e72679d932047d7ddaf0
xs30snw <e4n22s@gmail.com>
2021-10-09 22:52:55 +0500

    Changed Square.js from class to function component

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,10 @@
-import React from 'react';
-
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick() }
-            >
-                { this.props.value }
-            </button>
-        );
-    }
+export default function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            { props.value }
+        </button>
+    )
 }
-
-export default Square;
