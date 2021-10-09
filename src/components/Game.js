66907c225915ce8d08ef30ed78cc71f1673ae2b1
xs30snw <e4n22s@gmail.com>
2021-10-09 22:54:46 +0500

    Separated calculateWinner function from Game to be imported as a utils component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Board from '../components/Board';
+import calculateWinner from '../components/calculateWinner';
 
 export class Game extends React.Component {
     constructor(props) {
@@ -75,26 +76,6 @@ export class Game extends React.Component {
             </div>
         );
     }
-}
-
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
 }
 
 export default Game
